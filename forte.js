a4a66a468e98b5f14f5761a23a157554cb312033
Dev <86075203+web-co1@users.noreply.github.com>
2024-02-13 16:50:20 +0500

    Create forte.js

---
+++ forte.js
@@ -0,0 +1,110 @@
+<script>
+var SliderWhiteCombination = {
+        white_white: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca83906827a3f6e1d158fa_FULL_WHITE%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca849a95c94dc17df4366b_WHITE_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca8541996ca9e0bf6dbf88_WHITE_CAMERA_3%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85a01d5db8c769c78ef4_WHITE_CAMERA_4%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85e58538d7ea3bbc762b_WHITE_CAMERA_5%20(1).png",
+        ],
+        white_brown: [
+           "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca83906827a3f6e1d158fa_FULL_WHITE%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca849a95c94dc17df4366b_WHITE_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca8541996ca9e0bf6dbf88_WHITE_CAMERA_3%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85a01d5db8c769c78ef4_WHITE_CAMERA_4%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85e58538d7ea3bbc762b_WHITE_CAMERA_5%20(1).png",
+        ],
+        white_dark: [
+           "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca83906827a3f6e1d158fa_FULL_WHITE%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca849a95c94dc17df4366b_WHITE_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca8541996ca9e0bf6dbf88_WHITE_CAMERA_3%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85a01d5db8c769c78ef4_WHITE_CAMERA_4%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85e58538d7ea3bbc762b_WHITE_CAMERA_5%20(1).png",
+        ],
+        white_green: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca83906827a3f6e1d158fa_FULL_WHITE%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca849a95c94dc17df4366b_WHITE_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca8541996ca9e0bf6dbf88_WHITE_CAMERA_3%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85a01d5db8c769c78ef4_WHITE_CAMERA_4%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65ca85e58538d7ea3bbc762b_WHITE_CAMERA_5%20(1).png",
+        ],
+      };
+
+      var SliderBrownCombination = {
+        brown_white: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa019c27c1be4cfd45e60_FULL_BROWN_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa10c5f1d7197cc6642b7_FULL_BROWN_CAMERA_3%20(1).png",
+           "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa17aed62890b2dc7c053_FULL_BROWN%20(1).png",
+           "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa1e2e109f07d78d42315_FULL_BROWN_CAMERA_5%20(1).png",
+           "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa22f4108d5d844476f6f_FULL_BROWN_CAMERA_4%20(1).png",
+   
+        ],
+        brown_brown: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6be224b7888879fe53426_kcfbb%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6bf0e7dc685a293e84b46_kcfbbf%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6bfa2d0e53d5dca8bd787_kcfbbth%20(1).jpg",
+        ],
+        brown_dark: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6c3e542d05747954c302b_kcfbd%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6c4b2914bfe659f1cf0f5_kcfbdt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6c4fb3c02c2b945c0fc71_kcfbdth%20(1).jpg",
+        ],
+        brown_green: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6c6f102d264387bbdbc88_kcfbggo%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6c7cc22d7a73f46a2c05e_kcfbgt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6c8285d8fbef438ec57b7_kcfbgth%20(1).jpg",
+        ],
+      };
+
+      var SliderDarkCombination = {
+       // dumy img for dark white
+       dark_white: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa3ab0016d76abc6883d7_FULL_BLUE_%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa42122a5676baa354138_BLUE_CAMERA_3%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa4894108d5d84448ea8c_BLUE_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa4d2b46f3c587b660584_BLUE_CAMERA_5%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa52df333892b51c74b6e_BLUE_CAMERA_4%20(1).png",
+        ],
+        dark_brown: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6cd0ade35e6f500fcb208_fcfbg%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6cd627509c8cb434a84a1_fcgbgt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6cdeb952f122d4754c7f9_fcgbgth%20(1).jpg",
+        ],
+        dark_dark: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6d038457546181f96f018_fcfdd%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6d0e52946316fcd212700_fcfddt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6d13917cfb9f8435c78a2_fcfddth%20(1).jpg",
+        ],
+        dark_green: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6d365e039f3c502b9d86f_kcfdg%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6d44e723f67efc241b69f_kcfdgt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c6d53c61a615d2c0c52050_kcfdgth%20(1).jpg",
+        ],
+      };
+
+      var SliderGreenCombination = {
+        green_white: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa5ede1cd6c60ad0943aa_FULL_GREEN%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa63815d15e8bb9931b45_GREE_CAMERA_5%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa688c29036f298d34a1f_GREE_CAMERA_4%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa6d9c8f47e0518dcf9ce_GREE_CAMERA_2%20(1).png",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65caa72dd50cd61915407c8d_GREE_CAMERA_3%20(1).png",
+        ],
+        green_brown: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c79c26706f68a4f1b77fbb_kcfgb%20(2)%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c79c52f4ec4e1316aed01b_kcfgbt%20(1)%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c79c6713a4de2e155470c9_kcfgbth%20(1)%20(1).jpg",
+        ],
+        green_dark: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c79ebc29efedb4d3f7508d_kcfgd%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c79eea3acbd57cd1a28c89_kcfgdt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c79efe0c3fd9c058c5c740_kcfgdth.jpg",
+        ],
+        green_green: [
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c7a245e59f3578cae69097_kcfgg%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c7a29389cca18d71e58fb0_kcfggt%20(1).jpg",
+          "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c7a307f4ec4e1316b21dec_kcfggth%20(1).jpg",
+
+        ],
+      };
+</script>
